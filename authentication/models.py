a75e191440db2de2e2f358cea54f1fb428ff45d8
Nemesix493 <verquerre.kevin@gmail.com>
2023-09-18 18:04:49 +0200

    feat (authentication): add user and weighing model

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
+class User(AbstractUser):
+    birthdate = models.DateField(
+        verbose_name='Date of birth',
+    )
 
+
+class Weighing(models.Manager):
+    weight = models.FloatField(
+        verbose_name='Weight'
+    )
+    date = models.DateTimeField(
+        verbose_name='Weighing date',
+        auto_now_add=True
+    )
+    user = models.ForeignKey(
+        to=get_user_model(),
+        related_name='weighings',
+        related_query_name='weighings'
+    )
