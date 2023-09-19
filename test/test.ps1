a39538ea1e70fb4c033218a607f2ec29a9d0d9d7
Nemesix493 <verquerre.kevin@gmail.com>
2023-09-19 12:16:55 +0200

    test : add powershel file to automatize tests and exclude migration to flake8

---
+++ test/test.ps1
@@ -0,0 +1,2 @@
+& c:/Users/verqu/Documents/DevProject/FitnessManager/code/FitnessManager/env/Scripts/python.exe -m flake8
+& c:/Users/verqu/Documents/DevProject/FitnessManager/code/FitnessManager/env/Scripts/python.exe -m manage test
